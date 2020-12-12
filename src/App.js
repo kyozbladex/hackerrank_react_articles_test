7b0ab18a56de9de671de5173ffa0261ae0b56fa6
kyozbladex <72166766+kyozbladex@users.noreply.github.com>
2020-12-12 18:32:28 -0400

    listo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './App.css';
 import 'h8k-components';
 
@@ -8,15 +8,17 @@ const title = "Sorting Articles";
 
 function App({articles}) {
 
+    const articlesRef = useRef();
+
     return (
         <div className="App">
             <h8k-navbar header={title}></h8k-navbar>
             <div className="layout-row align-items-center justify-content-center my-20 navigation">
                 <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
+                <button data-testid="most-upvoted-link" className="small" onClick={() => { articlesRef.current.orderUpvotes() }}>Most Upvoted</button>
+                <button data-testid="most-recent-link" className="small" onClick={() => {articlesRef.current.orderDate()}}>Most Recent</button>
             </div>
-            <Articles articles={articles}/>
+            <Articles articles={articles} ref={articlesRef}/>
         </div>
     );
 
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, {useState, forwardRef, useImperativeHandle} from 'react';
 
-function Articles() {
+const Articles = forwardRef(({articles}, ref) => {
+
+
+    useImperativeHandle(
+        ref,
+        () => (
+            {orderUpvotes, orderDate}
+         ),
+     )
+
+    const arrayArticles = articles;
+
+    const [listData, setListData] = useState(arrayArticles);
+    
+    function orderUpvotes() {
+       
+        setListData([...listData].sort((a, b) => b.upvotes - a.upvotes ))
+                                               
+    }
+
+    function orderDate() {
+
+        setListData([...listData].sort((a, b) => new Date(b.date) - new Date(a.date)));
+   
+    }
 
     return (
         <div className="card w-50 mx-auto">
@@ -13,16 +37,27 @@ function Articles() {
                 </tr>
                 </thead>
                 <tbody>
-                <tr data-testid="article" key="article-index">
-                    <td data-testid="article-title">Article 1 title</td>
-                    <td data-testid="article-upvotes">Article 1 title</td>
-                    <td data-testid="article-date">Article 1 title</td>
+               
+                {listData.map((array, index) => 
+                
+                
+                <tr data-testid="article" key={"article-index", index}>
+                    
+                 
+                    <td data-testid="article-title">{array.title}</td>
+                    <td data-testid="article-upvotes">{array.upvotes}</td>
+                    <td data-testid="article-date">{array.date}</td>
+
+
                 </tr>
+                )}
+
+
                 </tbody>
             </table>
         </div>
     );
 
-}
+})
 
 export default Articles;
